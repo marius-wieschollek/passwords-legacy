52c627f7628b497be954432031d6193b5aba16bd
Marius David Wieschollek <passwords.public@mdns.eu>
2017-06-11 00:25:50 +0200

    Added DatabaseHelper

--- db/categorymapper.php
+++ db/categorymapper.php
@@ -1,29 +1,59 @@
 <?php
+
 namespace OCA\Passwords\Db;
 
-use \OCP\IDBConnection;
-use \OCP\AppFramework\Db\Mapper;
+use OCA\Passwords\Helper\DatabaseHelper;
+use OCP\AppFramework\Db\Mapper;
+use OCP\IDBConnection;
 
+/**
+ * Class CategoryMapper
+ *
+ * @package OCA\Passwords\Db
+ */
 class CategoryMapper extends Mapper {
 
-	public function __construct(IDBConnection $db) {
-		parent::__construct($db, 'passwords_categories', '\OCA\Passwords\Db\Category');
-	}
+    /**
+     * @var DatabaseHelper
+     */
+    private $databaseHelper;
 
-	public function find($id, $userId) {
-		$sql = 'SELECT * FROM *PREFIX*passwords_categories WHERE id = ? AND user_id = ?';
-		return $this->findEntity($sql, [$id, $userId]);
-	}
+    /**
+     * CategoryMapper constructor.
+     *
+     * @param IDBConnection $db
+     */
+    public function __construct(IDBConnection $db) {
+        $this->databaseHelper = new DatabaseHelper();
 
-	public function findAll($userId) {
-		$dbtype = \OC::$server->getConfig()->getSystemValue('dbtype', 'sqlite3');
-		if ($dbtype == 'mysql') {
-			$sql = 'SELECT * FROM *PREFIX*passwords_categories WHERE user_id = ? ORDER BY LOWER(category_name) ASC';
-		} else if ($dbtype == 'sqlite' OR $dbtype == 'sqlite3') {
-			$sql = 'SELECT * FROM *PREFIX*passwords_categories WHERE user_id = ? ORDER BY category_name COLLATE NOCASE';
-		} else {
-			$sql = 'SELECT * FROM *PREFIX*passwords_categories WHERE user_id = ? ORDER BY LOWER(category_name) ASC';
-		}
-		return $this->findEntities($sql, [$userId]);
-	}
+        parent::__construct($db, 'passwords_categories', '\OCA\Passwords\Db\Category');
+    }
+
+    /**
+     * @param $id
+     * @param $userId
+     *
+     * @return \OCP\AppFramework\Db\Entity
+     */
+    public function find($id, $userId) {
+        $sql = 'SELECT * FROM *PREFIX*passwords_categories WHERE id = ? AND user_id = ?';
+
+        return $this->findEntity($sql, [$id, $userId]);
+    }
+
+    /**
+     * @param $userId
+     *
+     * @return array
+     */
+    public function findAll($userId) {
+        $sql = 'SELECT * FROM *PREFIX*passwords_categories WHERE user_id = ? ORDER BY '.
+               $this->databaseHelper->getDatabaseDependentStatement([
+                   'mysql'   => "LOWER(category_name) COLLATE {$this->databaseHelper->getCollation()} ASC",
+                   'sqlite'  => 'category_name COLLATE NOCASE',
+                   'default' => 'LOWER(category_name) ASC'
+               ]);
+
+        return $this->findEntities($sql, [$userId]);
+    }
 }
