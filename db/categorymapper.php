3ba89ba305506023543a2c145c6fb1884ee64955
Marius David Wieschollek <passwords.public@mdns.eu>
2017-06-10 16:02:33 +0200

    Fix Nextcloud 12 database requests

--- db/categorymapper.php
+++ db/categorymapper.php
@@ -18,7 +18,7 @@ class CategoryMapper extends Mapper {
 	public function findAll($userId) {
 		$dbtype = \OC::$server->getConfig()->getSystemValue('dbtype', 'sqlite3');
 		if ($dbtype == 'mysql') {
-			$sql = 'SELECT * FROM *PREFIX*passwords_categories WHERE user_id = ? ORDER BY LOWER(category_name) COLLATE utf8mb4_general_ci ASC';
+			$sql = 'SELECT * FROM *PREFIX*passwords_categories WHERE user_id = ? ORDER BY LOWER(category_name) ASC';
 		} else if ($dbtype == 'sqlite' OR $dbtype == 'sqlite3') {
 			$sql = 'SELECT * FROM *PREFIX*passwords_categories WHERE user_id = ? ORDER BY category_name COLLATE NOCASE';
 		} else {
--- db/passwordmapper.php
+++ db/passwordmapper.php
@@ -88,7 +88,7 @@ class PasswordMapper extends Mapper {
 
 		// order by website according to database used
 		if ($dbtype == 'mysql') {
-			$sql = $sql . ' ORDER BY LOWER(website) COLLATE utf8mb4_general_ci ASC';
+			$sql = $sql . ' ORDER BY LOWER(website) ASC';
 		} else if ($dbtype == 'sqlite' OR $dbtype == 'sqlite3') {
 			$sql = $sql . ' ORDER BY website COLLATE NOCASE';
 		} else {
